79126b47c6289a98acd873571f214b29ce2b2467
pankass <87853664+pankass@users.noreply.github.com>
2024-10-29 18:53:05 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/absh
+#!/bin/bash
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -o server ./server/
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -o server ./client/
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -v -o server ./client.exe/
